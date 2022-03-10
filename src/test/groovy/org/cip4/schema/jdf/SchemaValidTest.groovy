ed889d479c811bdf82775acd1426f4c2b735f583
Michel Hartmann <michel.hartmann@flyeralarm.com>
2022-03-10 15:19:09 +0100

    Add possibility to validate the schema files
    Issues: IN-506

---
+++ src/test/groovy/org/cip4/schema/jdf/SchemaValidTest.groovy
@@ -0,0 +1,35 @@
+package org.cip4.schema.jdf
+
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+import javax.xml.XMLConstants
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
+
+class SchemaValidTest {
+
+    @ParameterizedTest
+    @MethodSource('schemas')
+    void productGetChildrenRawIsEmptyByDefault(File schema) {
+        def xsdUrl = new URL("https://www.w3.org/2001/XMLSchema.xsd")
+
+        xsdUrl.withInputStream { xsd ->
+            schema.withInputStream { xml ->
+                SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+                        .newSchema(new StreamSource(xsd, xsdUrl.toString()))
+                        .newValidator()
+                        .validate(new StreamSource(xml, schema.name))
+            }
+        }
+    }
+
+    static Collection<File> schemas() {
+        return new File('.').listFiles(new FilenameFilter() {
+            @Override
+            boolean accept(File dir, String name) {
+                return name.endsWith('.xsd')
+            }
+        })
+    }
+}
